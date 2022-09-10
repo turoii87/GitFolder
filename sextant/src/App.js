8e6f65398173a8937c39f7640e51cccb21f7582c
turoii87 <barretto.arturo@gmail.com>
2022-09-11 01:07:06 +0800

    StartPoint

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,20 +1,12 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Welcome to Sextant.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>sextant baby</h1>
+
     </div>
   );
 }
