d62e553b81b48b93cfaa8858ac89d7f87061ce53
turoii87 <barretto.arturo@gmail.com>
2022-09-12 20:17:56 +0800

    Try Welcome Dialog

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-
-      <h1>sextant baby</h1>
-
+    <div className={'App App-' + props.color}>
+      {props.children}
     </div>
   );
 }
+
+function WelcomeDialog() {
+  return (
+    <App color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting Sextant!
+      </p>
+    </App>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<WelcomeDialog />);
+
 
 export default App;
